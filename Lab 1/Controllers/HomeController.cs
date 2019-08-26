ee30cb09900bfd73a3a725f453aac687ce9fc929
Mario991012 <mvelasquezb99@gmail.com>
2019-08-25 23:03:53 -0600

    Vista de menu, lectura de archivo, separacion de archivo, almacenamiento de caracteres en diccionario completo

---
+++ Lab 1/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Lab_1.Singleton;
+
+namespace Lab_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        static bool SeCargoDiccionario = false;
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Carga()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Carga(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    string model = "";
+                    SeCargoDiccionario = true;
+                    model = Server.MapPath("~/Archivo.txt");
+                    file.SaveAs(model);
+                    if (Data.Instancia.Lectura(model) == 1)
+                    {
+                        ViewBag.Msg = "Carga del archivo correcta";
+                        ViewBag.Mensaje = "Carga del archivo correcta";
+                        return RedirectToAction("CargaCorrecta");
+                    }
+                    else
+                    {
+                        ViewBag.Msg = "Carga del archivo incorrecta";
+                        return RedirectToAction("CargaIncorrecta");
+                    }
+                }
+                else
+                {
+                    ViewBag.Msg = "ERROR AL CARGAR EL ARCHIVO, INTENTE DE NUEVO";
+                    return View();
+                }
+            }
+            catch
+            {
+                return ViewBag.Msg = "ERROR AL CARGAR EL ARCHIVO, INTENTE DE NUEVO";
+            }
+        }
+
+        public ActionResult CargaCorrecta()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult CargaIncorrecta()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Lab 1/Singleton/Data.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Collections;
+using Newtonsoft.Json;
+
+namespace Lab_1.Singleton
+{
+    public class Data
+    {
+        private static Data instancia = null;
+        public static Data Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Data();
+                }
+                return instancia;
+            }
+        }
+
+
+        public Dictionary<char, int> CodigosPrefijo = new Dictionary<char, int>();
+
+        public int Lectura(string path)
+        {
+            try
+            {
+                string[] lineas = File.ReadAllLines(path);
+                
+                foreach (var item in lineas)
+                {
+                    char[] caracteres = item.ToCharArray();
+                    for (int i = 0; i < caracteres.Length; i++)
+                    {
+                        if (CodigosPrefijo.ContainsKey(caracteres[i]) == false)
+                        {
+                            CodigosPrefijo.Add(caracteres[i], 1);
+                        }
+                        else
+                        {
+                            CodigosPrefijo[caracteres[i]] = CodigosPrefijo[caracteres[i]] + 1;
+                        }
+                    }
+                    
+                }
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        
+        //public Dictionary<string, string> PalabrasReservadasPredeterminadas = new Dictionary<string, string>();
+
+        //public string nombreTabla { get; set; }
+        //public List<string> NombresTabla = new List<string>();
+
+        //public Estructuras_de_Datos.Registro reg = new Estructuras_de_Datos.Registro();
+
+        //public List<Estructuras_de_Datos.NodoB<Estructuras_de_Datos.Registro>> listaNodos = new List<Estructuras_de_Datos.NodoB<Estructuras_de_Datos.Registro>>();
+
+        //public Dictionary<string, Estructuras_de_Datos.ArbolB<Estructuras_de_Datos.Registro>> Arboles = new Dictionary<string, Estructuras_de_Datos.ArbolB<Estructuras_de_Datos.Registro>>();
+        
+
+    }
+}
